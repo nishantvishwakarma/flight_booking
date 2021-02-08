f30a046d36eac4960d547b4d5a3cf5ba9dea6592
Nishant Vishwakarma <nishant.vishwakarma@intelegencia.com>
2021-02-08 14:25:44 +0530

    Changes for AutoLoading

--- AddPassenger.php
+++ AddPassenger.php
@@ -12,14 +12,19 @@ $this->age = $age;
 $this->gender = $gender;
 }
 public function addPassenger(){
-$fileName = time()._.$this->firstName.'.json';
+$fileName = $this->firstName.'.json';
 $data = array('first_name' => $this->firstName, 'last_name' => $this->lastName, 'age' => $this->age, 'gender' => $this->gender);
 $jsonData = json_encode($data);
 if (!file_exists($fileName)){
 fopen($fileName, "w");	
 }
 $saveDataToFile = file_put_contents($fileName, $jsonData);
-var_dump($saveDataToFile);exit;
+if ($saveDataToFile){
+	return "User Details Saved Successfully.\n";
+}
+else{
+	return "Can not saved user Details.\n";
+}
 }
 }
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
-include ('AddPassenger.php');
-
+include ('autoloading.php');
 function addPassenger($firstname, $lastname, $age, $gender){
 $addPass = new AddPassenger($firstname, $lastname, $age, $gender);
 $passInfo = $addPass->addPassenger();
+echo $passInfo;exit;
+}
+function fetchAirCraftInfo(){
+$airCraft = new AirCraft();
+$airCraftInfo = $airCraft->getAirCraftDetails();
+echo 'nishant';exit;
 }
 ?>
