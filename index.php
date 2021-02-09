73969547d6133d7d2adae0a2685e5f3bd344e809
Nishant Vishwakarma <nishant.vishwakarma@intelegencia.com>
2021-02-09 14:09:28 +0530

    Get Seat Availability

--- index.php
+++ index.php
@@ -16,8 +16,21 @@ $seatAssign = $airCraft->assignSeatsToPassenger($firstname.'.json');
 $getSeats = $airCraft->getSeats();
 foreach ($getSeats as $key => $value) {
 	foreach ($value as $keys => $values) {
-		
+		// print_r($value);exit;
+		if ($keys == 'airbus_a380'){
+		if ($values->first_class > 0){
+			echo $keys.' Flight assigned to ' . $firstname. ' in first class.';exit;
+		}
+		elseif ($values->business > 0) {
+			echo 'Boeing 737 800 Flight assigned to ' . $firstname. ' in business class.';exit;
+		}
+	}
 	}
 	}	
+}
+function getSeatsAvailability(){
+$airCraft = new AirCraft();
+$getSeats = $airCraft->getSeats();
+print_r($getSeats);exit;	
 }
 ?>
